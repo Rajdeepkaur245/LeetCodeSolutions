568cc193ab736b189013596a43c1fb22d7cf63a5
Rajdeepkaur245 <34265037+Rajdeepkaur245@users.noreply.github.com>
2020-06-14 16:46:01 +0530

    Update MultiThreadSquares.java

--- Java/MultiThreadSquares.java
+++ Java/MultiThreadSquares.java
@@ -1,31 +1,48 @@
+/*
+ * Q:1 Write a program explaining multiple threads working together to solve a problem:       
+ *    1. Create a int[] DataArray of 100 elements with random numbers.
+ *    2. Create 5 threads which take the part of elements from DataArray.
+ *    3. Those threads should compute the Square[ ] and Cubes[ ] for those respective values.
+ *    4. Print result of square and cubes after the all process threads complete their execution.
+ *
+ */
+
 import java.util.*;
-public class MultiThreadSquares{
+public class MultiThreadSquares {
     
     private static int[] initiliazeArray(int n) {
         int[] arr = new int[n];
         Random r = new Random();
         for (int i=0; i<n; i++) {
-            arr[i] = r.nextInt(10);
+            arr[i] = r.nextInt(1000);
         }
         return arr;
     }
+    
+    private static void printResult(int[] arr, int[] squares, int[] cubes) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println("Square of " + arr[i] + " is " + squares[i] + ".");
+            System.out.println("Cube of " + arr[i] + " is " + cubes[i] + ".");
+            System.out.println();
+        }
+    }
 
-     public static void main(String []args) throws InterruptedException{
-        int n = 100;
+     public static void main(String []args) throws InterruptedException {
+        int n = 100, threadCount = 5;
+        
         int[] arr = initiliazeArray(n);
         
-        int[] squares= new int[n];
+        int[] squares = new int[n];
         int[] cubes = new int[n];
         
-        int threadCount = 3;
         ComputeThread[] threadArray = new ComputeThread[Math.min(threadCount, n)];
-        
+    
         int start = 0;
         int rangeSize = (int) Math.ceil((double) n / threadArray.length);
         for(int i = 0; i < threadArray.length; i++) {
             int end = start + rangeSize - 1;
-            threadArray[i] = new ComputeThread(arr, squares, cubes, start, Math.min(end, n));
-            start = start + rangeSize;
+            threadArray[i] = new ComputeThread(arr, squares, cubes, start, Math.min(end, n - 1));
+            start = end + 1;
             threadArray[i].start();
         }
         
@@ -33,11 +50,7 @@ public class MultiThreadSquares{
             threadArray[i].join();
         }
         
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println("Square of " + arr[i] + " is " + squares[i] + ".");
-            System.out.println("Cube of " + arr[i] + " is " + cubes[i] + ".");
-            System.out.println();
-        }
+        printResult(arr, squares, cubes);
      }
 }
 
